30b7c01df7213fc099055fe7572ae63ddc38f9c6
Ava Hoffman <avamariehoffman@gmail.com>
2021-02-05 15:04:06 -0500

    Add another data source

--- src/config.R
+++ src/config.R
@@ -1,6 +1,9 @@
 # Data files
-urbanization_cover_data <- 
+urbanization_roads_data <- 
   "data/NLCD_2016_Impervious_descriptor/NLCD_2016_Impervious_descriptor_L48_20190405.img"
+
+urbanization_cover_data <- 
+  "data/NLCD_2016_Impervious/NLCD_2016_Impervious_L48_20190405.img"
 
 field_site_data <- 
   "data/macrosystems_urban_site_data.csv"
