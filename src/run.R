9c3d9a34375fc7ac81816a8f23f3e9d7bad86e63
Ava Hoffman <avamariehoffman@gmail.com>
2021-02-27 16:25:56 -0500

    Remove unfunctionalized code

--- src/run.R
+++ src/run.R
@@ -15,14 +15,3 @@ setwd(
 # Load source code
 load("src/config.R")
 load("src/sample_map.R")
-
-BA_urbancov <- get_envt_data_from_site(urban_data = r_BA, sites = s_BA)
-BO_urbancov <- get_envt_data_from_site(urban_data = r_BO, sites = s_BO) #50% under 20
-MN_urbancov <- get_envt_data_from_site(urban_data = r_MN, sites = s_MN) #43% under 20
-PX_urbancov <- get_envt_data_from_site(urban_data = r_PX, sites = s_PX)
-LA_urbancov <- get_envt_data_from_site(urban_data = r_LA, sites = s_LA)
-
-d <- 
-  rbind(BA_urbancov, BO_urbancov, MN_urbancov, PX_urbancov, LA_urbancov) %>%
-  dplyr::select(city, urban_pct)
-
