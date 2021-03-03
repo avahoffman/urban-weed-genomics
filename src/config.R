a9e8be486cef52a75f91a926de4f89767e442367
Ava Hoffman <avamariehoffman@gmail.com>
2021-03-03 08:40:17 -0500

    Add paths for redlining and projection data

--- src/config.R
+++ src/config.R
@@ -5,12 +5,17 @@ urbanization_roads_data <-
 urbanization_cover_data <- 
   "data/NLCD_2016_Impervious/NLCD_2016_Impervious_L48_20190405.img"
 
+redlining_data <- 
+  "/data/Redline_data//holc_ad_data.shp"
+
 field_site_data <- 
   "data/macrosystems_urban_site_data.csv"
 
 samples_data <- 
   "data/macrosystems_urban_leaf_samples_FINAL.xlsx"
 
+data_projection_settings <- 
+  "data/aea_projection_crs_class.Rdata"
 
 # outfile locations
 nlcd_pct_urban_by_site_out <- 
