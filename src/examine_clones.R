fd85083dba39a3a92a876ef93d78332c6d723c7d
AM Hoffman <avamariehoffman@gmail.com>
2022-03-21 17:53:24 -0400

    Update and add supplement doc

--- src/examine_clones.R
+++ src/examine_clones.R
@@ -2,32 +2,39 @@ library(ggplot2)
 library(tidyr)
 library(magrittr)
 
-# Read in output from the clone filtering part of sequence preperation
-df <- readr::read_csv("output/clone_filter_out.csv") %>%
-  pivot_longer(
-    cols = c(input, output, PCR_clones_removed),
-    names_to = "feature",
-    values_to = "value"
-  )
 
-# Reorder factor and add plotworthy names
-df$feature <-
-  factor(
-    df$feature,
-    levels = c("input", "PCR_clones_removed", "output"),
-    labels = c(
-      "Reads Input (per sublibrary)",
-      "PCR clones removed",
-      "Reads Output (per sublibrary)"
+make_cloneplot <- function() {
+  # Read in output from the clone filtering part of sequence preperation
+  df <- readr::read_csv("output/clone_filter_out.csv") %>%
+    pivot_longer(
+      cols = c(input, output, PCR_clones_removed),
+      names_to = "feature",
+      values_to = "value"
     )
-  )
-
-# Create plot
-ggplot(data = df) +
- geom_density(aes(x = value), fill = "#2171b5", alpha = 0.6) +
-  facet_wrap(~feature, ncol = 1, scales = "free") +
-  theme_minimal() +
-  xlab(NULL)
-
-# Write plot
-ggsave("figures/pcr_clones.jpg")
+  
+  # Reorder factor and add plotworthy names
+  df$feature <-
+    factor(
+      df$feature,
+      levels = c("input", "PCR_clones_removed", "output"),
+      labels = c(
+        "Reads Input (per sublibrary)",
+        "PCR clones removed",
+        "Reads Output (per sublibrary)"
+      )
+    )
+  
+  # Create plot
+  gg <- ggplot(data = df) +
+    geom_density(aes(x = value), fill = "#2171b5", alpha = 0.6) +
+    facet_wrap( ~ feature, ncol = 1, scales = "free") +
+    theme_minimal() +
+    xlab(NULL)
+  gg
+  
+  # Write plot
+  ggsave("figures/pcr_clones.jpg")
+  
+  return(gg)
+  
+}
