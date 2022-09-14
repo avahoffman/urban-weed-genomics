0b3568c3a2cf3a16c85d37667570a712730d1040
avahoffman <avamariehoffman@gmail.com>
2022-09-14 13:21:20 -0400

    Add catalogs

---
+++ bash/11-city_catalogs/PA_LA_22_09_14/05a-denovo_map.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Need  ~15 mins per sample
+#SBATCH --time=3:00:00
+#SBATCH --job-name=denovo_map
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=avamariehoffman@gmail.com
+#SBATCH --error=denovo_map_PA_LA.%j.out
+#SBATCH --output=denovo_map_PA_LA.%j.out
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+# 24 cores = a single node
+#SBATCH --ntasks-per-node=8
+
+
+# Written by: Ava Hoffman
+# Date:       14 September 2022
+# Purpose:    Running denovo_map.pl to execute the Stacks pipeline on a subset of samples for \
+#             parameter selection - doing this for PA species
+# denovo_map.pl --samples dir --popmap path -o dir [--paired [--rm-pcr-duplicates]] \
+#       (assembly options) (filtering options) [-X prog:"opts" ...]
+
+module load gcc
+module load bioperl
+export PATH=$PATH:/home/ahoffm31/scratch4-mavolio2/code_AH/stacks/bin/
+export PATH=$PATH:/home/ahoffm31/scratch4-mavolio2/code_AH/stacks-2.62/
+
+echo "Creating a by-city catalog for Poa (PA) in Los Angeles."
+
+denovo_map.pl -M 5 -T 8 -o /home/ahoffm31/scratch4-mavolio2/scratch_AH/catalog_tests/PA_LA_22_09_14 \
+  --popmap /home/ahoffm31/scratch4-mavolio2/scratch_AH/catalog_tests/PA_LA_22_09_14/popmap_PA_LA.txt \
+  --samples /home/ahoffm31/scratch4-mavolio2/scratch_AH/demux/PA --paired --time-components
+
