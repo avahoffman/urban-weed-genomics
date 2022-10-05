6ce47b66c3493f09de2e45e8aabb431c9621b1d7
avahoffman <avamariehoffman@gmail.com>
2022-10-05 11:50:50 -0400

    Switch to Png and code cleanup

--- src/config.R
+++ src/config.R
@@ -20,6 +20,9 @@ data_projection_settings <-
 
 ##########
 # outfile locations
+nlcd_spatial_data <- 
+  "data/macrosystems_urban_spatial_data.rds"
+
 nlcd_pct_urban_by_site_out <- 
   "data/macrosystems_urban_site_data_nlcd_pct_urban_by_site.csv"
 
