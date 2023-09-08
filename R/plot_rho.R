17829d423e68d9bb1f3798b10050158ca41db70f
avahoffman <avamariehoffman@gmail.com>
2023-09-08 13:10:30 -0400

    Prelim rho stuff

--- R/plot_rho.R
+++ R/plot_rho.R
@@ -1,30 +1,51 @@
 # library(adegenet)
 # library(hierfstat)
 # library(poppr)
+library(reshape2)
 
-read_dat <- function() {
+read_rhos_dat <- function() {
   CD <- 
-    read.csv("output/population_stats/genodive_output_rho_CD.csv", row.names = 1)
+    read.csv("output/population_stats/genodive_output_rho_CD.csv")
   DS <-
-    read.csv("output/population_stats/genodive_output_rho_DS.csv", row.names = 1)
+    read.csv("output/population_stats/genodive_output_rho_DS.csv")
   EC <-
-    read.csv("output/population_stats/genodive_output_rho_EC.csv", row.names = 1)
+    read.csv("output/population_stats/genodive_output_rho_EC.csv")
   LS <-
-    read.csv("output/population_stats/genodive_output_rho_LS.csv", row.names = 1)
+    read.csv("output/population_stats/genodive_output_rho_LS.csv")
   PA <-
-    read.csv("output/population_stats/genodive_output_rho_PA.csv", row.names = 1)
+    read.csv("output/population_stats/genodive_output_rho_PA.csv")
   TO <-
-    read.csv("output/population_stats/genodive_output_rho_TO.csv", row.names = 1)
+    read.csv("output/population_stats/genodive_output_rho_TO.csv")
   
   rhos_ <- list(
-    "CD" = as.matrix(CD),
-    "DS" = as.matrix(DS),
-    "EC" = as.matrix(EC),
-    "LS" = as.matrix(LS),
-    "PA" = as.matrix(PA),
-    "TO" = as.matrix(TO)
+    "CD" = CD,
+    "DS" = DS,
+    "EC" = EC,
+    "LS" = LS,
+    "PA" = PA,
+    "TO" = TO
   )
   
   return(rhos_)
 }
 
+
+rhos <- read_rhos_dat()
+
+rho_i <- rhos$EC
+rownames(rho_i) <- rho_i$X 
+rho_i <- rho_i[,-1]
+rho_i[upper.tri(rho_i)] <- NA
+rho_i[rho_i == 0] <- NA
+rho_i$v1 <- rownames(rho_i)
+rho_i <- melt(rho_i)
+
+ggplot(rho_i, aes(v1, variable)) +
+  theme_minimal() +
+  geom_tile(aes(fill = value), color='white') +
+  scale_fill_gradient(low = 'white', high = 'darkblue', space = 'Lab', limit = c(0,0.5)) +
+  theme(axis.text.x=element_text(angle=90),
+        axis.ticks=element_blank(),
+        axis.line=element_blank(),
+        panel.border=element_blank(),
+        panel.grid.major=element_line(color='#eeeeee'))
