5af8f023e139661a0bb87e95710eba3e1b8a2fa5
avahoffman <avamariehoffman@gmail.com>
2025-08-01 13:19:16 -0400

    Move file and finalize plot dimensions

--- R/10-Fig2-wrangle-climate-normals.R
+++ R/10-Fig2-wrangle-climate-normals.R
@@ -20,7 +20,8 @@ get_climate_normals <- function(city_ = "BA", temp_ = T) {
     `MLY-TMIN-NORMAL` = (`MLY-TMIN-NORMAL` - 32) * 5 / 9
   ) %>% mutate(# Convert to mm
     `MLY-PRCP-NORMAL` = `MLY-PRCP-NORMAL` * 25.4,
-    `MLY-SNOW-NORMAL` = `MLY-SNOW-NORMAL` * 25.4,) %>%
+    `MLY-SNOW-NORMAL` = `MLY-SNOW-NORMAL` * 25.4,
+  ) %>%
     pivot_longer(
       c(
         `MLY-PRCP-NORMAL`,
@@ -30,17 +31,30 @@ get_climate_normals <- function(city_ = "BA", temp_ = T) {
         `MLY-TMIN-NORMAL`
       )
     ) %>%
-    dplyr::filter(city == city_)
+    dplyr::filter(city == city_) %>%
+    mutate(DATE = as.integer(DATE))
   
   if (temp_) {
     gg <-
       cn %>%
       dplyr::filter(name %in% c("MLY-TAVG-NORMAL", "MLY-TMAX-NORMAL", "MLY-TMIN-NORMAL")) %>%
-      ggplot(aes(x = DATE, y = value, color = name)) +
+      ggplot(aes(
+        x = DATE,
+        y = value,
+        color = name,
+        group = name
+      )) +
       geom_line() +
-      scale_color_manual(values = c("#e36829", "#9d0309", "#fcbf1c")) +
+      scale_color_manual(
+        values = c("#e36829", "#9d0309", "#fcbf1c"),
+        labels = c("Average", "Maximum", "Minimum")
+      ) +
       ylim(c(-15, 43)) +
-      theme_classic()
+      scale_x_continuous(breaks = c(3.5, 6.5, 9.5), labels = c(3, 6, 9)) +
+      xlab("Month") +
+      ylab("Temperature (C)") +
+      theme_classic() +
+      theme(legend.title = element_blank())
     gg
   } else {
     gg <-
@@ -48,9 +62,16 @@ get_climate_normals <- function(city_ = "BA", temp_ = T) {
       dplyr::filter(name %in% c("MLY-PRCP-NORMAL", "MLY-SNOW-NORMAL")) %>%
       ggplot(aes(x = DATE, y = value, color = name)) +
       geom_line() +
-      scale_color_manual(values = c("#476e91", "#5cdaf8")) +
+      scale_color_manual(
+        values = c("#476e91", "#5cdaf8"),
+        labels = c("Precipitation", "Snow")
+      ) +
       ylim(c(-1, 400)) +
-      theme_classic()
+      scale_x_continuous(breaks = c(3.5, 6.5, 9.5), labels = c(3, 6, 9)) +
+      xlab("Month") +
+      ylab("Precipitation / snow (mm)") +
+      theme_classic() +
+      theme(legend.title = element_blank())
     gg
   }
   
@@ -58,17 +79,17 @@ get_climate_normals <- function(city_ = "BA", temp_ = T) {
 
 
 make_normals_all_cities <- function() {
-  g1 <- get_climate_normals(city_ = "BA", temp_ = T)
-  g2 <- get_climate_normals(city_ = "BO", temp_ = T)
-  g3 <- get_climate_normals(city_ = "LA", temp_ = T)
-  g4 <- get_climate_normals(city_ = "MN", temp_ = T)
-  g5 <- get_climate_normals(city_ = "PX", temp_ = T)
+  g1 <- get_climate_normals(city_ = "BA", temp_ = T) + xlab(NULL)
+  g2 <- get_climate_normals(city_ = "BO", temp_ = T) + ylab(NULL) + xlab(NULL) + theme(axis.text.y = element_blank())
+  g3 <- get_climate_normals(city_ = "LA", temp_ = T) + ylab(NULL) + xlab(NULL) + theme(axis.text.y = element_blank())
+  g4 <- get_climate_normals(city_ = "MN", temp_ = T) + ylab(NULL) + xlab(NULL) + theme(axis.text.y = element_blank())
+  g5 <- get_climate_normals(city_ = "PX", temp_ = T) + ylab(NULL) + xlab(NULL) + theme(axis.text.y = element_blank())
   
-  g6 <- get_climate_normals(city_ = "BA", temp_ = F)
-  g7 <- get_climate_normals(city_ = "BO", temp_ = F)
-  g8 <- get_climate_normals(city_ = "LA", temp_ = F)
-  g9 <- get_climate_normals(city_ = "MN", temp_ = F)
-  g10 <- get_climate_normals(city_ = "PX", temp_ = F)
+  g6 <- get_climate_normals(city_ = "BA", temp_ = F) + xlab("")
+  g7 <- get_climate_normals(city_ = "BO", temp_ = F) + ylab(NULL) + xlab("") + theme(axis.text.y = element_blank())
+  g8 <- get_climate_normals(city_ = "LA", temp_ = F) + ylab(NULL) + theme(axis.text.y = element_blank())
+  g9 <- get_climate_normals(city_ = "MN", temp_ = F) + ylab(NULL) + xlab("") + theme(axis.text.y = element_blank())
+  g10 <- get_climate_normals(city_ = "PX", temp_ = F) + ylab(NULL) + xlab("") + theme(axis.text.y = element_blank())
   
   legend1 <- get_legend(g3)
   legend2 <- get_legend(g8)
@@ -95,9 +116,9 @@ make_normals_all_cities <- function() {
   mega_plot
   
   ggsave(
-    paste0("figures/Fig2_sampling_map/climate_norms_temp_ALL.png"),
+    paste0("figures/Fig2_sampling_map/climate_norms_ALL.png"),
     dpi = "print",
-    height = 2,
+    height = 3.5,
     width = 11,
     units = "in"
   )
