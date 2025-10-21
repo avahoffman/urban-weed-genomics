046fa45bd33732e190c0b3f88cebe3ee2047fbb3
avahoffman <avamariehoffman@gmail.com>
2025-10-21 11:23:52 -0400

    Formatting throughout

--- R/15-IBD_IBE_MMRR.R
+++ R/15-IBD_IBE_MMRR.R
@@ -1,7 +1,7 @@
 library(algatr)    # [github::TheWangLab/algatr] v1.0.0 # %>% / mmrr_run / mmrr_table
 library(adegenet)  # CRAN v2.1.10 # as.matrix
 library(cowplot)   # CRAN v1.1.3 # plot_grid
-library(tidyverse) # CRAN v2.0.0 
+library(tidyverse) # CRAN v2.0.0
 
 
 spp_labels <- function() {
@@ -20,16 +20,20 @@ spp_labels <- function() {
 
 # First goal is to get the genetic distance matrix -- by site within city
 # Need to correct the structure file to reflect this.
-correct_structure_files <- function(spp_){
+correct_structure_files <- function(spp_) {
   infile_stem <- paste0("SNP_data/", spp_, "/", spp_, "_estimatedgeno")
   struct_ <-
-    read_delim(paste0(infile_stem, ".structure")) %>% 
+    read_delim(paste0(infile_stem, ".structure")) %>%
     rename(SampleName = `...1`, pop = `...2`)
   struct_ <-
-    struct_ %>% 
-    mutate(pop = as.numeric(as.factor(str_replace(struct_$SampleName, "..$", "")))) %>% 
+    struct_ %>%
+    mutate(pop = as.numeric(as.factor(
+      str_replace(struct_$SampleName, "..$", "")
+    ))) %>%
     arrange(pop)
-  write_delim(struct_, paste0(infile_stem, "_sitesaspops.structure"), delim = "\t")
+  write_delim(struct_,
+              paste0(infile_stem, "_sitesaspops.structure"),
+              delim = "\t")
   
   file <- readLines(paste0(infile_stem, "_sitesaspops.structure"))
   file[1] <- stringr::str_remove_all(file[1], "SampleName|pop")
@@ -37,31 +41,35 @@ correct_structure_files <- function(spp_){
   # Chord distance is used in genodive
 }
 
-get_gen_dist_matrix <- function(spp_){
+get_gen_dist_matrix <- function(spp_) {
   infile_stem <- paste0("SNP_data/", spp_, "/", spp_, "_estimatedgeno")
   names_ <-
-    read_delim(paste0(infile_stem, ".structure")) %>% 
-    rename(SampleName = `...1`, pop = `...2`) %>% 
+    read_delim(paste0(infile_stem, ".structure")) %>%
+    rename(SampleName = `...1`, pop = `...2`) %>%
     mutate(pop = str_replace(SampleName, "..$", "")) %>%
-    arrange(pop) %>% 
-    distinct(pop) %>% 
+    arrange(pop) %>%
+    distinct(pop) %>%
     pull(pop)
   
-  mat_ <- read_delim(paste0("output/genodive/", spp_, "_genetic_distance_bysite_rho.txt"))
+  mat_ <- read_delim(paste0(
+    "output/genodive/",
+    spp_,
+    "_genetic_distance_bysite_rho.txt"
+  ))
   mat_ <- as.matrix(mat_ %>% dplyr::select(-`...1`), labels = T)
   colnames(mat_) <- rownames(mat_) <- names_
   mat_ <- as.dist(mat_)
   return(mat_)
 }
 
-get_geo_dist_matrix <- function(spp_){
+get_geo_dist_matrix <- function(spp_) {
   infile_stem <- paste0("SNP_data/", spp_, "/", spp_, "_estimatedgeno")
   names_ <-
-    read_delim(paste0(infile_stem, ".structure")) %>% 
-    rename(SampleName = `...1`, pop = `...2`) %>% 
+    read_delim(paste0(infile_stem, ".structure")) %>%
+    rename(SampleName = `...1`, pop = `...2`) %>%
     mutate(pop = str_replace(SampleName, "..$", "")) %>%
-    arrange(pop) %>% 
-    distinct(pop) %>% 
+    arrange(pop) %>%
+    distinct(pop) %>%
     dplyr::select(pop)
   
   # Get lat long from the site info
@@ -71,8 +79,8 @@ get_geo_dist_matrix <- function(spp_){
     read_csv("data/site_data_urban_cov.csv") %>%
     mutate(site_abbv = str_replace(site_abbv, " ", "_"))  %>%
     unite("site_abbv", city_abbv, site_abbv, management_type, sep = ".") %>%
-    mutate(pop = paste0(spp_,".",site_abbv))
-
+    mutate(pop = paste0(spp_, ".", site_abbv))
+  
   d_geo <-
     names_ %>%
     left_join(site_info) %>%
@@ -91,38 +99,38 @@ get_geo_dist_matrix <- function(spp_){
 get_env_dist_matrix_list <- function(spp_) {
   infile_stem <- paste0("SNP_data/", spp_, "/", spp_, "_estimatedgeno")
   names_ <-
-    read_delim(paste0(infile_stem, ".structure")) %>% 
-    rename(SampleName = `...1`, pop = `...2`) %>% 
+    read_delim(paste0(infile_stem, ".structure")) %>%
+    rename(SampleName = `...1`, pop = `...2`) %>%
     mutate(pop = str_replace(SampleName, "..$", "")) %>%
-    arrange(pop) %>% 
-    distinct(pop) %>% 
+    arrange(pop) %>%
+    distinct(pop) %>%
     dplyr::select(pop)
   
-  # Get the site environmental info 
+  # Get the site environmental info
   # Make sure to include management type as otherwise site names aren't unique
   # Get the site info
   site_info <-
-    read_csv("data/site_data_DUC_environvars.csv") 
-  site_info <- 
+    read_csv("data/site_data_DUC_environvars.csv")
+  site_info <-
     site_info %>%
-    dplyr::select(site_abbv,
-                  city_abbv,
-                  management_type,
-                  nlcd_urban_pct,
-                  distance_to_city_center,
-                  soiltemp_2.5cm_Apr_12pm,
-                  soiltemp_2.5cm_Jul_12pm
-                  ) %>%
+    dplyr::select(
+      site_abbv,
+      city_abbv,
+      management_type,
+      nlcd_urban_pct,
+      distance_to_city_center,
+      soiltemp_2.5cm_Apr_12pm,
+      soiltemp_2.5cm_Jul_12pm
+    ) %>%
     mutate(site_abbv = str_replace(site_abbv, " ", "_"))  %>%
     unite("site_abbv", city_abbv, site_abbv, management_type, sep = ".") %>%
-    mutate(pop = paste0(spp_,".",site_abbv)) %>% 
-    rename(soiltemp_Apr = soiltemp_2.5cm_Apr_12pm,
-           soiltemp_Jul = soiltemp_2.5cm_Jul_12pm)
+    mutate(pop = paste0(spp_, ".", site_abbv)) %>%
+    rename(soiltemp_Apr = soiltemp_2.5cm_Apr_12pm, soiltemp_Jul = soiltemp_2.5cm_Jul_12pm)
   
   env_vars <- colnames(site_info)[2:5]
   
   env_list <- list()
-  for(var in env_vars){
+  for (var in env_vars) {
     d_env <-
       names_ %>%
       left_join(site_info) %>%
@@ -133,17 +141,21 @@ get_env_dist_matrix_list <- function(spp_) {
     Denv <- dist(d_env)
     env_list[[var]] <- Denv
   }
-
+  
   return(env_list)
 }
 
 
-do_MMRR <- function(spp_){
+do_MMRR <- function(spp_) {
   Y <- get_gen_dist_matrix(spp_)
   X <- get_env_dist_matrix_list(spp_)
   X[["geodist"]] <- get_geo_dist_matrix(spp_)
   set.seed(444)
-  results_full <- mmrr_run(Y, X, nperm = 9999, stdz = TRUE, model = "full")
+  results_full <- mmrr_run(Y,
+                           X,
+                           nperm = 9999,
+                           stdz = TRUE,
+                           model = "full")
   out_ <- mmrr_table(results_full, digits = 2, summary_stats = TRUE)
   out_ <- out_[["_data"]]
   out_$spp <- spp_
@@ -151,9 +163,8 @@ do_MMRR <- function(spp_){
 }
 
 
-make_mmrr_plot <- function(){
-  
-  if(!file.exists("output/MMRR/MMRR_total.csv")) {
+make_mmrr_plot <- function() {
+  if (!file.exists("output/MMRR/MMRR_total.csv")) {
     mmrr_ <- rbind(
       do_MMRR("CD"),
       do_MMRR("DS"),
@@ -167,16 +178,17 @@ make_mmrr_plot <- function(){
     mmrr_ <- readr::read_csv("output/MMRR/MMRR_total.csv")
   }
   
-  if(!file.exists("output/MMRR/MMRR_total_overall_models.csv")) {
+  if (!file.exists("output/MMRR/MMRR_total_overall_models.csv")) {
     p_vals_overall_models <-
-      mmrr_ %>% filter(var %in% c("R-Squared:", "F-Statistic:", "F p-value:")) %>% 
-      dplyr::select(var, estimate, spp) %>% 
+      mmrr_ %>% filter(var %in% c("R-Squared:", "F-Statistic:", "F p-value:")) %>%
+      dplyr::select(var, estimate, spp) %>%
       pivot_wider(names_from = var, values_from = estimate)
-    readr::write_csv(p_vals_overall_models, "output/MMRR/MMRR_total_overall_models.csv")
+    readr::write_csv(p_vals_overall_models,
+                     "output/MMRR/MMRR_total_overall_models.csv")
   } else {
     p_vals_overall_models <- readr::read_csv("output/MMRR/MMRR_total_overall_models.csv")
   }
-    
+  
   mmrr_plot_ <-
     mmrr_ %>% filter(var != "F-Statistic:" &
                        var != "F p-value:" & var != "Intercept") %>%
@@ -199,10 +211,7 @@ make_mmrr_plot <- function(){
   heat_plot <- function(in_dat, params = T) {
     plot_ <-
       ggplot() +
-      geom_tile(
-        data = in_dat ,
-        aes(x = Species, y = var, fill = estimate)
-      ) +
+      geom_tile(data = in_dat , aes(x = Species, y = var, fill = estimate)) +
       geom_text(
         data = in_dat ,
         size = 2,
@@ -212,7 +221,7 @@ make_mmrr_plot <- function(){
       theme(axis.text.x = element_text(angle = 45, hjust = 1),
             text = element_text(size = 8)) +
       labs(x = NULL, y = NULL)
-   
+    
     if (params) {
       plot_ <- plot_ + scale_fill_gradient2(
         limit = c(-2, 1.5),
@@ -234,7 +243,8 @@ make_mmrr_plot <- function(){
     plot_ <-
       plot_ +
       ggnewscale::new_scale_fill() +
-      geom_tile(data = in_dat %>% filter(spp != "EC"), aes(x = Species, y = var, fill = estimate)) +
+      geom_tile(data = in_dat %>% filter(spp != "EC"),
+                aes(x = Species, y = var, fill = estimate)) +
       geom_text(
         data = in_dat %>% filter(spp != "EC"),
         size = 2,
@@ -252,45 +262,55 @@ make_mmrr_plot <- function(){
     return(plot_)
   }
   
-  env_plot_ <- 
+  env_plot_ <-
     heat_plot(mmrr_plot_ %>% filter(facet_ == "Environment"))  +
     labs(y = "Model variable") +
-    scale_y_discrete(labels = c("Distance to city center", "% Impervious", "April soil temp.", "July soil temp."))  +
+    scale_y_discrete(
+      labels = c(
+        "Distance to city center",
+        "% Impervious",
+        "April soil temp.",
+        "July soil temp."
+      )
+    )  +
     theme(legend.title = element_blank(), axis.text.x = element_blank()) +
     guides(fill = FALSE)
-  dist_plot_ <- 
+  dist_plot_ <-
     heat_plot(mmrr_plot_ %>% filter(facet_ == "Distance")) +
     scale_y_discrete(labels = c("Geographic distance")) +
     theme(legend.title = element_blank(), axis.text.x = element_blank()) +
     theme(legend.position = "none")
-  r2_plot_ <- 
+  r2_plot_ <-
     heat_plot(mmrr_plot_ %>% filter(var == "R-Squared"), params = F) +
     guides(fill = FALSE) +
-    theme(legend.title = element_blank(), 
+    theme(legend.title = element_blank(),
           axis.text.x = ggtext::element_markdown())
-    
+  
   
   gg <- cowplot::plot_grid(
     env_plot_,
     dist_plot_,
     r2_plot_,
     ncol = 1,
-    rel_heights = c(1,0.35, 0.85),
+    rel_heights = c(1, 0.35, 0.85),
     align = "v"
   )
   
   gg
   
   # Prepare figures such that, after reduction to fit across one column, two-thirds page width, or two columns (80 mm, 112 mm, or 169 mm, respectively) as required, all lettering and symbols will be clear and easy to read,
-  ggsave(paste0("figures/Fig7_MMRR/MMRR_total.png"),
-         dpi = "print",
-         height = 80,
-         width = 112,
-         units = "mm") 
+  ggsave(
+    paste0("figures/Fig7_MMRR/MMRR_total.png"),
+    dpi = "print",
+    height = 80,
+    width = 112,
+    units = "mm"
+  )
 }
 
-do_MMRR_by_city <- function(spp_, city_){
-  print(spp_); print(city_)
+do_MMRR_by_city <- function(spp_, city_) {
+  print(spp_)
+  print(city_)
   
   # Get full matrices
   Y <- get_gen_dist_matrix(spp_)
@@ -299,12 +319,12 @@ do_MMRR_by_city <- function(spp_, city_){
   
   # Filter matrices
   Y_city <- as.matrix(Y, labels = T)
-  labels_to_keep <- labels(Y_city)[[1]][str_detect(labels(Y_city)[[1]], paste0(spp_,".",city_))]
+  labels_to_keep <- labels(Y_city)[[1]][str_detect(labels(Y_city)[[1]], paste0(spp_, ".", city_))]
   Y_city <- Y_city[rownames(Y_city) %in% labels_to_keep, colnames(Y_city) %in% labels_to_keep]
   Y_city <- as.dist(Y_city)
   
   X_city <- list()
-  for(item in names(X)){
+  for (item in names(X)) {
     X_temp_ <- X[[item]]
     X_temp_ <- as.matrix(X_temp_, labels = T)
     X_temp_ <- X_temp_[rownames(X_temp_) %in% labels_to_keep, colnames(X_temp_) %in% labels_to_keep]
@@ -312,7 +332,11 @@ do_MMRR_by_city <- function(spp_, city_){
     X_city[[item]] <- X_temp_
   }
   set.seed(444)
-  results_full <- mmrr_run(Y_city, X_city, nperm = 999, stdz = TRUE, model = "full")
+  results_full <- mmrr_run(Y_city,
+                           X_city,
+                           nperm = 999,
+                           stdz = TRUE,
+                           model = "full")
   out_ <- mmrr_table(results_full, digits = 2, summary_stats = TRUE)
   out_ <- out_[["_data"]]
   out_$spp <- spp_
@@ -320,7 +344,7 @@ do_MMRR_by_city <- function(spp_, city_){
   return(out_)
 }
 
-make_mmrr_plot_bycity_ <- function(){
+make_mmrr_plot_bycity_ <- function() {
   mmrr_city_ <- rbind(
     do_MMRR_by_city("CD", "BA"),
     do_MMRR_by_city("CD", "LA"),
@@ -351,78 +375,125 @@ make_mmrr_plot_bycity_ <- function(){
   readr::write_csv(mmrr_city_, "output/MMRR/MMRR_splitbycity.csv")
   
   p_vals_overall_models <-
-    mmrr_city_ %>% filter(var %in% c("R-Squared:", "F-Statistic:", "F p-value:")) %>% 
-    dplyr::select(var, estimate, spp, city) %>% 
+    mmrr_city_ %>% filter(var %in% c("R-Squared:", "F-Statistic:", "F p-value:")) %>%
+    dplyr::select(var, estimate, spp, city) %>%
     pivot_wider(names_from = var, values_from = estimate)
   
   p_vect <- pull(p_vals_overall_models, `F p-value:`)
   
   p_vals_overall_models$`p adjusted` <- round(c(
-    p.adjust(p_vect[1:3], method = "BH"), #CD
-    p.adjust(p_vect[4:7], method = "BH"), #DS
-    p.adjust(p_vect[8:10], method = "BH"), #EC
-    p.adjust(p_vect[11:15], method = "BH"), #LS
-    p.adjust(p_vect[16:19], method = "BH"), #PA
+    p.adjust(p_vect[1:3], method = "BH"),
+    #CD
+    p.adjust(p_vect[4:7], method = "BH"),
+    #DS
+    p.adjust(p_vect[8:10], method = "BH"),
+    #EC
+    p.adjust(p_vect[11:15], method = "BH"),
+    #LS
+    p.adjust(p_vect[16:19], method = "BH"),
+    #PA
     p.adjust(p_vect[20:24], method = "BH")  #TO
-  ), 4)
+  ),
+  4)
   
-  readr::write_csv(p_vals_overall_models, "output/MMRR/MMRR_splitbycity_overall_models.csv")
+  readr::write_csv(p_vals_overall_models,
+                   "output/MMRR/MMRR_splitbycity_overall_models.csv")
   
-  mmrr_plot_city_ <- 
-    mmrr_city_ %>% filter(var != "F-Statistic:" & var != "F p-value:" & var != "Intercept") %>% 
+  mmrr_plot_city_ <-
+    mmrr_city_ %>% filter(var != "F-Statistic:" &
+                            var != "F p-value:" & var != "Intercept") %>%
     mutate(
       estimate = case_when(p > 0.05 ~ NA, TRUE ~ estimate),
-      facet_ = case_when(var == "geodist" ~ "Distance", var %in% c("distance_to_city_center", "nlcd_urban_pct", "soiltemp_Apr", "soiltemp_Jul") ~ "Environment"),
+      facet_ = case_when(
+        var == "geodist" ~ "Distance",
+        var %in% c(
+          "distance_to_city_center",
+          "nlcd_urban_pct",
+          "soiltemp_Apr",
+          "soiltemp_Jul"
+        ) ~ "Environment"
+      ),
       var = case_when(var == "R-Squared:" ~ "R-Squared", TRUE ~ var),
       estimate_rounded = round(estimate, digits = 2),
       spp_city = paste0(spp, ".", city)
     )
   
-  env_plot_ <- 
-    ggplot(mmrr_plot_city_ %>% filter(facet_ == "Environment"), aes(x = spp_city, y = var)) +
+  env_plot_ <-
+    ggplot(mmrr_plot_city_ %>% filter(facet_ == "Environment"),
+           aes(x = spp_city, y = var)) +
     geom_tile(aes(fill = estimate)) +
     geom_text(aes(label = estimate_rounded)) +
-    scale_fill_gradient2(limit = c(-2,1.5), low = "red", high =  "blue", mid = "white", midpoint = 0, na.value = "white") +
-    scale_y_discrete(labels = c("Distance to city center", "% Urban cover", "April soil temp.", "July soil temp.")) +
+    scale_fill_gradient2(
+      limit = c(-2, 1.5),
+      low = "red",
+      high =  "blue",
+      mid = "white",
+      midpoint = 0,
+      na.value = "white"
+    ) +
+    scale_y_discrete(
+      labels = c(
+        "Distance to city center",
+        "% Urban cover",
+        "April soil temp.",
+        "July soil temp."
+      )
+    ) +
     theme_classic() +
     labs(y = "Model variable", x = NULL) +
     theme(legend.title = element_blank(), axis.text.x = element_blank())
   
-  dist_plot_ <- 
-    ggplot(mmrr_plot_city_ %>% filter(facet_ == "Distance"), aes(x = spp_city, y = var)) +
+  dist_plot_ <-
+    ggplot(mmrr_plot_city_ %>% filter(facet_ == "Distance"),
+           aes(x = spp_city, y = var)) +
     geom_tile(aes(fill = estimate)) +
     geom_text(aes(label = estimate_rounded)) +
-    scale_fill_gradient2(limit = c(-2,1.5), low = "red", high =  "blue", mid = "white", midpoint = 0, na.value = "white") +
+    scale_fill_gradient2(
+      limit = c(-2, 1.5),
+      low = "red",
+      high =  "blue",
+      mid = "white",
+      midpoint = 0,
+      na.value = "white"
+    ) +
     scale_y_discrete(labels = c("Geographic distance")) +
     theme_classic() +
     theme(legend.position = "none", axis.text.x = element_blank()) +
     labs(x = NULL, y = NULL)
   
-  r2_plot_ <- 
-    ggplot(mmrr_plot_city_ %>% filter(var == "R-Squared"), aes(x = spp_city, y = var)) +
+  r2_plot_ <-
+    ggplot(mmrr_plot_city_ %>% filter(var == "R-Squared"),
+           aes(x = spp_city, y = var)) +
     geom_tile(aes(fill = estimate)) +
     geom_text(aes(label = estimate_rounded)) +
-    scale_fill_gradient2(limit = c(-2,1.5), low = "red", high =  "blue", mid = "white", midpoint = 0, na.value = "white") +
+    scale_fill_gradient2(
+      limit = c(-2, 1.5),
+      low = "red",
+      high =  "blue",
+      mid = "white",
+      midpoint = 0,
+      na.value = "white"
+    ) +
     theme_classic() +
     theme(legend.position = "none") +
-    labs(x = "Species + city combination", y = NULL) 
+    labs(x = "Species + city combination", y = NULL)
   
   gg <- cowplot::plot_grid(
     env_plot_,
     dist_plot_,
     r2_plot_,
     ncol = 1,
-    rel_heights = c(1,0.35, 0.5),
+    rel_heights = c(1, 0.35, 0.5),
     align = "v"
   )
   
   gg
   
-  ggsave(paste0("figures/Fig7_MMRR/MMRR_splitbycity.png"),
-         dpi = "print",
-         height = 4,
-         width = 12,
-         units = "in") 
+  ggsave(
+    paste0("figures/Fig7_MMRR/MMRR_splitbycity.png"),
+    dpi = "print",
+    height = 4,
+    width = 12,
+    units = "in"
+  )
 }
-
-
